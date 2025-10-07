e82a1241510689bb90af4db8b31410049a10fc07
Ира Высоцкая <irina.o.vysotskaya@gmail.com>
2025-10-07 12:08:53 +0300

    о да первый треугольник

---
+++ get-dac/8-bit_triangle.py
@@ -0,0 +1,21 @@
+import r2r_dac as r2r
+import signal_generator as sg
+import time
+
+try: 
+    amplitude = 2
+    signal_frequency = 10
+    sampling_frequency = 500
+    dac = r2r.R2R_DAC([16, 20, 21, 25, 26, 17, 27, 22], 3.183, True)
+
+    while True:
+        try:
+            t = time.time()
+            voltage = sg.get_triangle_amplitude(signal_frequency, t)
+            dac.set_voltage(voltage*amplitude)
+            sg.wait_for_sampling_period(sampling_frequency)
+        except ValueError:
+            print("Вы ввели не число. Попробуйте ещё раз\n")
+
+finally: 
+    dac.deinit()
--- get-dac/signal_generator.py
+++ get-dac/signal_generator.py
@@ -7,5 +7,12 @@ def get_sin_wave_amplitude(freq, time):
     s = s/2
     return s
 
+def get_triangle_amplitude(freq, time):
+    T = 1/freq
+    x = time%T
+    if x<=T/2:
+        return (2/T)*x
+    return 1-(2/T)*(x-T/2)
+
 def wait_for_sampling_period(sampling_frequency):
     time.sleep(1/sampling_frequency)
