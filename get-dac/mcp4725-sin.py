dd2dea27bb2399c848d8c8f81a2462877cd72411
Ира Высоцкая <irina.o.vysotskaya@gmail.com>
2025-10-07 11:15:47 +0300

    господи я счастлива

--- get-dac/mcp4725-sin.py
+++ get-dac/mcp4725-sin.py
@@ -1,21 +1,22 @@
-import r2r_dac as r2r
+import mcp4725_driver as mcpdr
 import signal_generator as sg
 import time
+
 
 try: 
     amplitude = 3.2
     signal_frequency = 10
-    sampling_frequency = 100
-    dac = r2r.R2R_DAC([16, 20, 21, 25, 26, 17, 27, 22], 3.183, True)
+    sampling_frequency = 500
+    mcp = mcpdr.MCP4725(5.0, 0x61, True)
 
     while True:
         try:
             t = time.time()
             voltage = sg.get_sin_wave_amplitude(signal_frequency, t)
-            dac.set_voltage(voltage*amplitude)
+            mcp.set_voltage(voltage*amplitude)
             sg.wait_for_sampling_period(sampling_frequency)
         except ValueError:
             print("Вы ввели не число. Попробуйте ещё раз\n")
 
 finally: 
-    dac.deinit()
+    mcp.deinit()
