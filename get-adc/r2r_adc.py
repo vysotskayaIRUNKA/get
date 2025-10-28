f30c15963e9c85a6a303762ea2d6936ddbe0f0f0
Ира Высоцкая <irina.o.vysotskaya@gmail.com>
2025-10-28 10:18:21 +0300

    ох полетели

--- get-adc/r2r_adc.py
+++ get-adc/r2r_adc.py
@@ -38,13 +38,30 @@ class R2R_ADC:
         value = R2R_ADC.sequential_counting_acd(self)
         return (value/255)*self.dynamic_range
     
+    def successive_approximation_adc(self):
+        right = 256
+        left = 0
+        value = 0
+        while (right-left>1):
+            value = (right+left)//2
+            R2R_ADC.number_to_dac(self, value)
+            time.sleep(self.compare_time)
+            if (GPIO.input(self.comp_gpio)==1): 
+                right=value 
+            else: 
+                left=value 
+        return value
+    
+    def get_sar_voltage(self):
+        value = R2R_ADC.successive_approximation_adc(self)
+        return (value/255)*self.dynamic_range
+    
 
 if __name__ == "__main__":
     try:
-        adc = R2R_ADC(3.2, 0.01, False)
-        #adc.sequential_counting_acd()
+        adc = R2R_ADC(3.2, 0.1)
         while True:
-            voltage = adc.get_sc_voltage()
+            voltage = adc.get_sar_voltage()
             print(f"Напряжение: {voltage}")
     finally:
         adc.deinit()
