78093b956541e4a2d6b786ee6207485a810ab045
Irunka <irina.o.vysotskaya@gmail.com>
2025-11-18 09:46:16 +0300

    памагите

---
+++ лаба/mcp3021_driver.py
@@ -0,0 +1,44 @@
+import smbus
+import time
+import numpy as np
+
+class MCP3021:
+    def __init__(self, dynamic_range, verbose=False):
+        self.bus = smbus.SMBus(1)
+        self.dynamic_range = dynamic_range 
+        self.address = 0x4D
+        self.verbose = verbose 
+    
+    def deinit(self):
+        self.bus.close()
+    
+    def get_number(self):
+        data = self.bus.read_word_data(self.address, 0)
+        lower_data_byte = data >> 8
+        upper_data_byte = data & 0xFF 
+        number = (upper_data_byte << 6) | (lower_data_byte >> 2)
+        if self.verbose:
+            print(f"Принятые данные: {data}, Старший байт: {upper_data_byte:x}, Младший байт: {lower_data_byte:x}, Число:{number}")
+        return number 
+    
+    def get_voltage(self):
+        value = MCP3021.get_number(self)
+        return value/1024 * self.dynamic_range
+    
+
+if __name__ == "__main__":
+    mcp = MCP3021(5)
+    start = time.time()
+    try:
+        voltages = np.array([])
+        times = np.array([])
+        while time.time()-start < 50:
+            voltage = mcp.get_voltage()
+            t = time.time() - start
+            voltages.append(voltage)
+            times.append(t)
+            time.sleep(1)
+        data = np.column_stack(voltages, times)
+        np.savetxt('200mm.csv', data, delimiter=',', fmt='%.4f', header='Напряжение[B], Время[c]', comments='', encoding='utf-8')
+    finally:
+        mcp.deinit()
