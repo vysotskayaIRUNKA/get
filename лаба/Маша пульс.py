3d8927f81bdd1c95a602feb098e8cfd4e079d98c
IRUNKA <irina.o.vysotskaya@gmail.com>
2025-11-19 20:07:20 +0300

    расчет упругости сосудов

--- лаба/Маша пульс.py
+++ лаба/Маша пульс.py
@@ -41,6 +41,36 @@ cutoff_freq = 25
 
 filtered_signal, b, a = butterworth_filter(detrended_pressures, fs, cutoff_freq)
 
+pressures_for_1_puls = []
+times_for_1_puls = []
+for i in range(len(times)):
+    if times[i]>=23.05 and times[i]<=23.8:
+        times_for_1_puls.append(times[i])
+        pressures_for_1_puls.append(filtered_signal[i])
+
+peak1 = []
+times_peak1 = []
+for i in range(len(times_for_1_puls)):
+    if times_for_1_puls[i]>23.15 and times_for_1_puls[i]<23.2:
+        peak1.append(pressures_for_1_puls[i])
+        times_peak1.append(times_for_1_puls[i])
+sistola = max(peak1)
+time_sistola = [times_peak1[i] for i in range(len(peak1)) if peak1[i]==sistola]
+#время высокого пика -- 23.1862
+
+peak2 = []
+times_peak2 = []
+for i in range(len(times_for_1_puls)):
+    if times_for_1_puls[i]>23.4:
+        peak2.append(pressures_for_1_puls[i])
+        times_peak2.append(times_for_1_puls[i])
+distola = max(peak2)
+time_distola = [times_peak2[i] for i in range(len(peak2)) if peak2[i]==distola]
+#время низкого пика -- 23.4302
+
+RI = 1.63/(max(time_distola)-max(time_sistola))
+print(RI)
+
 plt.grid()
-plt.scatter(times, filtered_signal)
+plt.scatter(times_for_1_puls, pressures_for_1_puls)
 plt.show()
