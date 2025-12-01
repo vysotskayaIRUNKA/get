2cc176b3f9d9e6fa7ed2a92888846d9594fbc292
IRUNKA <irina.o.vysotskaya@gmail.com>
2025-12-01 21:55:20 +0300

    файлик для снятия данных

---
+++ лаба затопленная струя/jetMeasure.py
@@ -0,0 +1,21 @@
+import jetFunctions as j
+import spidev
+import matplotlib.pyplot as plt 
+
+spi = spidev.SpiDev()
+spi.open(0, 0)
+spi.max_speed_hz = 1600000
+
+def getAdc():
+    adcResponse = spi.xfer2([0, 0])
+    return ((adcResponse[0] & 0x1F) << 8 | adcResponse[1]) >> 1
+
+try: 
+    samples = []
+    for i in range(20000):
+        samples.append(getAdc())
+        plt.plot(samples)
+        plt.show()
+    
+finally:
+    spi.close()
